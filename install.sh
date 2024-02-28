c81d4640311ba3aed69eb21238d3c68440e459ce
Andrey Martemianov <andrey.zloy@gmail.com>
2024-02-29 01:00:54 +0300

    hm

--- install.sh
+++ install.sh
@@ -4,23 +4,28 @@ ln -sf $PWD/.bashrc $HOME/.bashrc
 ln -sf $PWD/.profile $HOME/.profile
 ln -sf $PWD/.dircolors $HOME/.dircolors
 
-# .config
+# bat
 mkdir -p ~/.config/bat
 ln -sf $PWD/.config/bat/config $HOME/.config/bat/config
 
+# htop
 mkdir -p ~/.config/htop
 ln -sf $PWD/.config/htop/htoprc $HOME/.config/htop/htoprc
 
+# imv
 mkdir -p ~/.config/imv
 ln -sf $PWD/.config/imv/config $HOME/.config/imv/config
 
+# kitty
 mkdir -p ~/.config/kitty
 ln -sf $PWD/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 ln -sf $PWD/.config/kitty/theme.conf $HOME/.config/kitty/theme.conf
 
+# nnn
 mkdir -p ~/.config/nnn
 ln -sf $PWD/.config/nnn/fcolors.bash $HOME/.config/nnn/fcolors.bash
 mkdir -p ~/.local/bin
 ln -sf $PWD/.local/bin/nnn $HOME/.local/bin/nnn
 
+# hyprland
 ln -sf $PWD/.config/hypr $HOME/.config/hypr
