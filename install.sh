8035787195e17679b651cdfd330a2e91acb01340
Andrey Martemianov <andrey.zloy@gmail.com>
2024-02-29 02:30:05 +0300

    install

--- install.sh
+++ install.sh
@@ -5,29 +5,30 @@ ln -sf $PWD/.profile $HOME/.profile
 ln -sf $PWD/.dircolors $HOME/.dircolors
 
 # bat
-mkdir -p ~/.config/bat
-ln -sf $PWD/.config/bat/config $HOME/.config/bat/config
+rm -r $HOME/.config/bat
+ln -sf $PWD/.config/bat $HOME/.config/bat
 
 # htop
-mkdir -p ~/.config/htop
-ln -sf $PWD/.config/htop/htoprc $HOME/.config/htop/htoprc
+rm -r $HOME/.config/htop
+ln -sf $PWD/.config/htop $HOME/.config/htop
 
 # imv
-mkdir -p ~/.config/imv
-ln -sf $PWD/.config/imv/config $HOME/.config/imv/config
+rm -r $HOME/.config/imv
+ln -sf $PWD/.config/imv $HOME/.config/imv
 
 # kitty
-mkdir -p ~/.config/kitty
-ln -sf $PWD/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
-ln -sf $PWD/.config/kitty/theme.conf $HOME/.config/kitty/theme.conf
+rm -r $HOME/.config/kitty
+ln -sf $PWD/.config/kitty $HOME/.config/kitty
 
 # nnn
-mkdir -p ~/.config/nnn
-ln -sf $PWD/.config/nnn/fcolors.bash $HOME/.config/nnn/fcolors.bash
+rm -r $HOME/.config/nnn
+ln -sf $PWD/.config/nnn $HOME/.config/nnn
+
 mkdir -p ~/.local/bin
 ln -sf $PWD/.local/bin/nnn $HOME/.local/bin/nnn
 
 # hyprland
+rm -r $HOME/.config/hypr
 ln -sf $PWD/.config/hypr $HOME/.config/hypr
 
 # oh-my-bash
@@ -35,4 +36,5 @@ rm -r $HOME/.oh-my-bash/custom/completions
 ln -sf $PWD/.oh-my-bash/custom/completions $HOME/.oh-my-bash/custom/
 
 # lsd
+rm -r $HOME/.config/lsd
 ln -sf $PWD/.config/lsd $HOME/.config/lsd
