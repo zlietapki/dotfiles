05ac1cef89e3ae1c3d9e0f45ae46d128cefc9150
Andrey Martemianov <andrey.zloy@gmail.com>
2024-03-30 01:40:09 +0300

    waybar modules

--- .config/waybar/start.sh
+++ .config/waybar/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Kill and restart waybar whenever its config files change
 
-CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css"
+CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css $HOME/.config/waybar/modules"
 
 trap "killall waybar" EXIT
 
