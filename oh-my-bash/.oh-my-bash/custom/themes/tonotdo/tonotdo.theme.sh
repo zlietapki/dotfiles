5940724da8f016d77e2d1dcc017823e927d2d770
Andrey Martemianov <andrey.zloy@gmail.com>
2025-08-18 01:20:38 +0300

    asd

--- oh-my-bash/.oh-my-bash/custom/themes/tonotdo/tonotdo.theme.sh
+++ oh-my-bash/.oh-my-bash/custom/themes/tonotdo/tonotdo.theme.sh
@@ -1,13 +1,14 @@
 #! bash oh-my-bash.module
 
+# SOLARIZED
 BASE03=$(tput setaf 234)
 BASE02=$(tput setaf 235)
 BASE01=$(tput setaf 240)
-BASE00=$(tput setaf 241)
+BASE00=$(tput setaf 241) # body text / default code / primary content
 BASE0=$(tput setaf 244)
-BASE1=$(tput setaf 245)
-BASE2=$(tput setaf 254)
-BASE3=$(tput setaf 230)
+BASE1=$(tput setaf 245) # comments / secondaty content
+BASE2=$(tput setaf 254) # backgroud highlights
+BASE3=$(tput setaf 230) # backgroud
 YELLOW=$(tput setaf 136)
 ORANGE=$(tput setaf 166)
 RED=$(tput setaf 160)
@@ -17,26 +18,63 @@ BLUE=$(tput setaf 33)
 CYAN=$(tput setaf 37)
 GREEN=$(tput setaf 64)
 
+BG_BASE2="$(tput setab 254)"
+
 OCEAN_BLUE1=$(tput setaf 39)
 OCEAN_BLUE2=$(tput setaf 69)
 OCEAN_BLUE3=$(tput setaf 105)
 
 BOLD=$(tput bold)
+UNDERLINE_START=$(tput smul)
+UNDERLINE_END=$(tput rmul)
+
 RESET=$(tput sgr0)
 
 # SCM_THEME_PROMPT_PREFIX=" ${_omb_prompt_purple}"
-SCM_THEME_PROMPT_PREFIX=" \[$VIOLET\]"
-SCM_THEME_PROMPT_SUFFIX=" ${_omb_prompt_normal}"
-SCM_THEME_PROMPT_DIRTY=" \[$RED\]✗"
-SCM_THEME_PROMPT_CLEAN=" \[$GREEN\]✓"
+SCM_THEME_PROMPT_PREFIX=" ${VIOLET}"
+SCM_THEME_PROMPT_SUFFIX=""
+SCM_THEME_PROMPT_DIRTY=" $RED✗"
+SCM_THEME_PROMPT_CLEAN=" $GREEN✓"
 SCM_GIT_SHOW_DETAILS="false"
 
 function _omb_theme_PROMPT_COMMAND() {
-  #PS1="${_omb_prompt_olive}\u${_omb_prompt_normal}${_omb_prompt_teal}@\h${_omb_prompt_normal}${_omb_prompt_purple} ${_omb_prompt_normal}${_omb_prompt_green}\w${_omb_prompt_normal}$(scm_prompt_info)> "
-  #PS1="${_omb_prompt_olive}\u${_omb_prompt_normal}${_omb_prompt_teal}@\h${_omb_prompt_normal}${_omb_prompt_purple} ${_omb_prompt_normal}${_omb_prompt_green}\w${_omb_prompt_normal}$(scm_prompt_info)> "
-  # PS1="\[$BOLD$GREEN\]\u\[$GREEN\]@\[$GREEN\]\h \[$BLUE\]\w\[$RESET\]$(scm_prompt_info)\[$RESET\]> "
-  # PS1="\[$BOLD$GREEN\]\u\[$YELLOW\]@\[$GREEN\]\h \[$BLUE\]\w\[$RESET\]$(scm_prompt_info)\[$RESET\]> "
-  PS1="\[$BOLD$OCEAN_BLUE1\]\u\[$OCEAN_BLUE2\]@\[$OCEAN_BLUE3\]\h \[$BLUE\]\w\[$RESET\]$(scm_prompt_info)\[$RESET\]> "
+  case $HOSTNAME in
+    'box')
+      ps_user_name="";
+      ps_user_at="";
+      ps_user_host="";
+      ps_user_path="${BLUE}\w";
+      
+      ps_user_mark=" ${BASE1}> ${RESET}";
+
+      ps_root_name="${RED}\u";
+      ps_root_at="";
+      ps_root_host="";
+      ps_root_path=" ${ORANGE}\w";
+      ps_root_mark=" ${ORANGE}# ${RESET}"
+    ;;
+    *)
+      ps_user_name="\u";
+      ps_user_at="@";
+      ps_user_host="\h";
+      ps_user_path=" ${CYAN}\w";
+      
+      ps_user_mark=" ${BASE1}> ${RESET}";
+
+      ps_root_name="${RED}\u";
+      ps_root_at="@";
+      ps_root_host="\h";
+      ps_root_path=" ${ORANGE}\w";
+      ps_root_mark=" ${ORANGE}# ${RESET}"
+  esac
+
+  case $(id -u) in
+    0) PS1="$ps_root_name$ps_root_at$ps_root_host$ps_root_path$(scm_prompt_info)$ps_root_mark${BASE00}"; # root
+        ;;
+    *) PS1="${BASE00}$ps_user_name$ps_user_at$ps_user_host$ps_user_path$(scm_prompt_info)$ps_user_mark${BASE00}";
+        ;;
+  esac
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
+PROMPT_DIRTRIM=0 # не сокращать полный путь папки
