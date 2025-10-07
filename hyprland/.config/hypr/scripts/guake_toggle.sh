a9e7adb7e5add99692fbe828d729e59c7eddf7b6
Andrey Martemianov <martemyanov.a11@rwb.ru>
2025-10-07 08:06:00 +0300

    useless script

--- hyprland/.config/hypr/scripts/guake_toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-hyprctl dispatch togglespecialworkspace guake
-
-#terminals_cnt=$(hyprctl clients -j | jq '[.[] | select(.workspace.name == "special:guake" and .class=="kitty")] | length')
-#if [ $terminals_cnt -eq 0 ]
-#then
-#    hyprctl dispatch exec [workspace special:guake] kitty
-#fi
