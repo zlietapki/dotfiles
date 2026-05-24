351266c0a667406c2397794447c91cf398e8295d
Andrey Martemianov <martemyanov.a11@rwb.ru>
2026-05-24 18:26:14 +0300

    Add .config/hypr/scripts/conditional_suspend.sh

---
+++ dot_config/hypr/scripts/executable_conditional_suspend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+RETRY_INTERVAL=${1:-3600}
+
+while true; do
+    DOWNLOADING=$(curl -sf --max-time 2 "http://127.0.0.1:8080/api/v2/torrents/info?filter=downloading")
+    if [ $? -eq 0 ] && [ "$DOWNLOADING" != "[]" ] && [ -n "$DOWNLOADING" ]; then
+        sleep "$RETRY_INTERVAL"
+    else
+        systemctl suspend
+        exit 0
+    fi
+done
